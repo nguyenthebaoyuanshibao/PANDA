d80c06bdaa68dd7a67230db61fd0fbf6ba8fc993
nguyenthebaoyuanshibao <batlieubattinh@gmail.com>
2016-04-04 03:55:53 +0000

    add comment

---
+++ app/views/image_comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @image_comment, :id, :user_id, :image_id, :comment, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330145558) do
+ActiveRecord::Schema.define(version: 20160404032956) do
+
+  create_table "image_comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "image_id"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.integer  "user_id"
